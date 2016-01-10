3ecf7ad759ae2409cc0a44c0e4a738ab92b7ab6a
Kakinger <jan@zueschen.eu>
2016-01-10 22:18:47 +0100

    Create site.mk

---
+++ site.mk
@@ -0,0 +1,39 @@
+GLUON_SITE_PACKAGES := \
+        gluon-mesh-batman-adv-15 \
+        gluon-alfred \
+        gluon-announced \
+        gluon-autoupdater \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-contact-info \
+        gluon-config-mode-core \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-hostname \
+        gluon-config-mode-tunneldigger \
+        gluon-ebtables-filter-multicast \
+        gluon-ebtables-filter-ra-dhcp \
+        gluon-ebtables-filter-roguenets \
+        gluon-luci-admin \
+        gluon-luci-autoupdater \
+        gluon-luci-portconfig \
+        gluon-luci-wifi-config \
+        gluon-luci-theme \
+        gluon-luci-private-wifi \
+        gluon-next-node \
+        gluon-mesh-vpn-tunneldigger \
+        gluon-radvd \
+        gluon-setup-mode \
+        gluon-status-page \
+        ffho-autoupdater-wifi-fallback \
+        ffho-txpower-fix \
+        ffho-wifi-blackout-workaround \
+        haveged \
+        iptables \
+        iwinfo
+
+DEFAULT_GLUON_RELEASE := 0.7.6
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?=en de
