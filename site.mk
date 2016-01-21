3df87977c3608c4b4264eba3e44c7c15729b362b
Kakinger <jan@zueschen.eu>
2016-01-21 21:57:27 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -11,26 +11,26 @@ GLUON_SITE_PACKAGES := \
         gluon-config-mode-tunneldigger \
         gluon-ebtables-filter-multicast \
         gluon-ebtables-filter-ra-dhcp \
-        gluon-ebtables-filter-roguenets \
+        gluon-luci-theme \
         gluon-luci-admin \
         gluon-luci-autoupdater \
-        gluon-luci-portconfig \
         gluon-luci-wifi-config \
-        gluon-luci-theme \
+        gluon-luci-portconfig \
         gluon-luci-private-wifi \
         gluon-next-node \
         gluon-mesh-vpn-tunneldigger \
         gluon-radvd \
         gluon-setup-mode \
         gluon-status-page \
-        ffho-autoupdater-wifi-fallback \
-        ffho-txpower-fix \
+        gluon-ssid-changer \
+        gluon-migrate-vpn \
         ffho-wifi-blackout-workaround \
-        haveged \
+        ffho-autoupdater-wifi-fallback \
+        iwinfo \
         iptables \
-        iwinfo
+        haveged
 
-DEFAULT_GLUON_RELEASE := 0.7.6
+DEFAULT_GLUON_RELEASE := 0.8-L2TP
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
