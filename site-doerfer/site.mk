990a54f159e292929527e2f2ebc42600ac7ea160
Kakinger <jan@zueschen.eu>
2017-07-11 19:06:47 +0200

    Update site.mk

--- site-doerfer/site.mk
+++ site-doerfer/site.mk
@@ -1,41 +1,80 @@
 GLUON_SITE_PACKAGES := \
-	gluon-ssid-notifier \
-	ff-wtbg-autoreboot\
         gluon-mesh-batman-adv-15 \
         gluon-alfred \
         gluon-respondd \
         gluon-autoupdater \
+	gluon-autorestart \
         gluon-config-mode-autoupdater \
         gluon-config-mode-contact-info \
         gluon-config-mode-core \
         gluon-config-mode-geo-location \
         gluon-config-mode-hostname \
-        gluon-config-mode-tunneldigger \
+        gluon-config-mode-mesh-vpn \
         gluon-ebtables-filter-multicast \
         gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-theme \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-wifi-config \
-        gluon-luci-portconfig \
-        gluon-luci-private-wifi \
-        gluon-next-node \
+      	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-wifi-config \
+	gluon-web-private-wifi \
         gluon-mesh-vpn-tunneldigger \
         gluon-radvd \
         gluon-setup-mode \
         gluon-status-page \
-        gluon-migrate-vpn \
         gluon-tunneldigger-watchdog \
-        ffho-wifi-blackout-workaround \
-        ffho-autoupdater-wifi-fallback \
         iwinfo \
         iptables \
         haveged
 
+USB_BASIC := \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid
+
+USB_NIC := \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-dm9601-ether
+
+USB_WIFI := \
+	kmod-rtl8192cu
+
+ifeq ($(GLUON_TARGET),x86-generic)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		kmod-usb-ohci-pci \
+		$(USB_NIC)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		kmod-igb #APU2
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		$(USB_WIFI)
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		$(USB_WIFI)
+endif
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+# Default priority for updates
 GLUON_PRIORITY ?= 0
+
 GLUON_LANGS ?=en de
+
 GLUON_REGION ?= eu
