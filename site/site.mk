de1f052947d809f3e466dcea3a66ee1ec6533eaa
Kakinger <jan@zueschen.eu>
2017-08-22 05:12:58 +0200

    removed IBSS

--- site/site.mk
+++ site/site.mk
@@ -33,7 +33,7 @@ GLUON_SITE_PACKAGES := \
         iptables \
         haveged
 
-DEFAULT_GLUON_RELEASE := 0.8.11.3
+DEFAULT_GLUON_RELEASE := 0.8.11.4
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
